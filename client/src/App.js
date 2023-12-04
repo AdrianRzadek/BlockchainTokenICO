4fa60921193d7530ceac92008d44c3d063938060
AdrianRzadek <mschxd@gmail.com>
2023-12-04 14:32:02 +0100

    update added null airdrop function

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,19 @@
-import React, { Component} from "react";
+import React, { Component } from "react";
 import { ethers } from "ethers";
 
 import DappToken from "./contracts/DappToken.json";
 import DappTokenSale from "./contracts/DappTokenSale.json";
 import Transactions from "./contracts/Transactions.json";
-
+import AirDrop from "./contracts/AirDrop.json";
 import contractAddress from "./contracts/contract-address.json";
 import "./App.scss";
 
 import ProgressLabel from "./Components/Progress";
-import  Loading from "./Components/Loading";
+import Loading from "./Components/Loading";
 //stan początkowy
 
-class App extends Component {
 
+class App extends Component {
   //konstruktor wartości kontraktu
   constructor(props) {
     super(props);
@@ -30,7 +30,8 @@ class App extends Component {
       balance: null,
       tokenDecimals: 0,
       tokenSymbol: "FOSSA",
-    };   
+      proof:null,
+    };
   }
 
   async componentDidMount() {
@@ -38,7 +39,6 @@ class App extends Component {
     await this.loadBlockchainData();
     await this.loadLogo();
     await this.polling();
-
   }
 
   async loadWeb3() {
@@ -47,10 +47,12 @@ class App extends Component {
 
       try {
         // Ethereum user detected. You can now use the provider.
-        const provider = await new ethers.BrowserProvider(await window.ethereum);
+        const provider = await new ethers.BrowserProvider(
+          await window.ethereum
+        );
 
         // Request account access if needed
-        await window.ethereum.request(await{method: "eth_requestAccounts" });
+        await window.ethereum.request(await { method: "eth_requestAccounts" });
 
         // We have access to the wallet
         const signer = await provider.getSigner();
@@ -79,12 +81,7 @@ class App extends Component {
 
   async loadBlockchainData() {
     if (typeof window.ethereum !== "undefined") {
-      const {
-        provider,
-        signer,
-        addressSigner,
-        tokenPrice,
-      } = await this.state;
+      const { provider, signer, addressSigner, tokenPrice } = await this.state;
 
       console.log(signer);
       console.log(provider);
@@ -92,7 +89,7 @@ class App extends Component {
       console.log(tokenPrice);
       console.log(DappTokenSale.abi);
 
-      if (DappTokenSale && DappToken && Transactions ) {
+      if (DappTokenSale && DappToken && Transactions && AirDrop) {
         const addressDappTokenSale = contractAddress.DappTokenSale;
         const abiDappTokenSale = DappTokenSale.abi;
         this.provider = await new ethers.BrowserProvider(window.ethereum);
@@ -120,15 +117,15 @@ class App extends Component {
           await this.provider.getSigner()
         );
 
-      /*  const addressExchange = contractAddress.Exchange;
-        const abiExchange = Exchange.abi;
+        const addressAirDrop = contractAddress.AirDrop;
+        const abiAirDrop = AirDrop.abi;
         this.provider = await new ethers.BrowserProvider(window.ethereum);
-        this.exchange = await new ethers.Contract(
-          await addressExchange,
-          await abiExchange,
+        this.airDrop = await new ethers.Contract(
+          await addressAirDrop,
+          await abiAirDrop,
           await this.provider.getSigner()
         );
-         console.log(this.exchange)*/
+        console.log(this.exchange);
         // console.log( await dappToken.transfer(dappTokenSale.target, this.state.tokensAvailable));
 
         // Load token sale data
@@ -146,13 +143,15 @@ class App extends Component {
 
         console.log(TokenPrice);
 
-        const TokensAvailable = await this.dappToken.balanceOf(this.dappTokenSale.target);
+        const TokensAvailable = await this.dappToken.balanceOf(
+          this.dappTokenSale.target
+        );
 
         console.log(TokensAvailable);
 
-       const TokensSold = await this.dappTokenSale.tokensSold();
-     //   console.log(TokensSold.toString());
-     //   console.log(TokensSold);
+        const TokensSold = await this.dappTokenSale.tokensSold();
+        //   console.log(TokensSold.toString());
+        //   console.log(TokensSold);
 
         //console.log(signer.address);
 
@@ -168,9 +167,8 @@ class App extends Component {
           tokensSold: TokensSold,
           addressDappToken,
           tokenPrice: TokenPrice,
-          tokensAvailable:TokensAvailable,
+          tokensAvailable: TokensAvailable,
         });
-     
       } else {
         window.alert("Smart contracts not deployed to the detected network.");
       }
@@ -178,52 +176,52 @@ class App extends Component {
   }
 
   async loadLogo() {
-    const tokenImage = "https://img.freepik.com/premium-zdjecie/akwarela-malarstwo-fossa_721965-64.jpg?w=826";
+    const tokenImage =
+      "https://img.freepik.com/premium-zdjecie/akwarela-malarstwo-fossa_721965-64.jpg?w=826";
     console.log(this.state.addressDappToken);
 
     try {
-        // Check local storage
-        
-        const tokenAdded = localStorage.getItem('tokenAdded');
-        const storedTokenAddress = localStorage.getItem('tokenAddress');
-        // If the token hasn't been added yet, prompt the user to add it
-        if (!tokenAdded || storedTokenAddress !== this.state.addressDappToken) {
-            const wasAdded = await window.ethereum.request({
-                method: "wallet_watchAsset",
-                params: {
-                    type: "ERC20",
-                    options: {
-                        address: this.state.addressDappToken,
-                        symbol: this.state.tokenSymbol,
-                        decimals: this.state.tokenDecimals,
-                        image: tokenImage,
-                    },
-                },
-            });
+      // Check local storage
 
-            if (wasAdded) {
-                console.log("Thanks for your interest!");
-                // Update local storage
-                localStorage.setItem('tokenAdded', 'true');
-                localStorage.setItem('tokenAddress', this.state.addressDappToken);
-              } else {
-                console.log("Your loss!");
-            }
+      const tokenAdded = localStorage.getItem("tokenAdded");
+      const storedTokenAddress = localStorage.getItem("tokenAddress");
+      // If the token hasn't been added yet, prompt the user to add it
+      if (!tokenAdded || storedTokenAddress !== this.state.addressDappToken) {
+        const wasAdded = await window.ethereum.request({
+          method: "wallet_watchAsset",
+          params: {
+            type: "ERC20",
+            options: {
+              address: this.state.addressDappToken,
+              symbol: this.state.tokenSymbol,
+              decimals: this.state.tokenDecimals,
+              image: tokenImage,
+            },
+          },
+        });
+
+        if (wasAdded) {
+          console.log("Thanks for your interest!");
+          // Update local storage
+          localStorage.setItem("tokenAdded", "true");
+          localStorage.setItem("tokenAddress", this.state.addressDappToken);
         } else {
-            // Here you can add code to check if the token is already added
-            console.log("Token already added or user previously prompted.");
+          console.log("Your loss!");
         }
+      } else {
+        // Here you can add code to check if the token is already added
+        console.log("Token already added or user previously prompted.");
+      }
     } catch (error) {
-        console.log(error);
+      console.log(error);
     }
-}
+  }
 
   buyTokens = async (event) => {
     event.preventDefault();
     // Prevent the default form submission behavior
 
-    const { tokenPrice} =
-      this.state;
+    const { tokenPrice } = this.state;
 
     try {
       // dappToken.transfer(dappTokenSale.target, tokensAvailable);
@@ -242,101 +240,92 @@ class App extends Component {
 
       const value = tokenPrice * numberOfTokensBigInt;
 
-    await this.dappTokenSale.buyTokens(numberOfTokensBigInt, {
+      await this.dappTokenSale.buyTokens(numberOfTokensBigInt, {
         address: this.state.addressSigner,
         value: value,
         gasLimit: 2000000,
-      })
-       
-      
+      });
+
       //this.setState({ loading: false, numberOfTokens: 0 }); // Reset the number of tokens
     } catch (error) {
       console.error(error);
       this.setState({ loading: false });
       console.log("blad");
     }
-
   };
- 
-
 
   Transfer = async (event) => {
     event.preventDefault();
     const reciver = await event.target.reciver.value;
-    const amount =  await event.target.amount.value;
-    const message =  await event.target.message.value; 
-   const value = await ethers.toBigInt(amount)
-     console.log(this.state.addressSigner);
-     console.log(reciver);
-     await this.dappToken.approve(this.transaction.target, value);
-     await this.transaction.sendTransaction( reciver,
-      value,
-      message,
-      {
-        from: this.state.addressSigner,
-        value: value,
-        gas: 200000000,
-      }
-      
-    )
-  }
+    const amount = await event.target.amount.value;
+    const message = await event.target.message.value;
+    const value = await ethers.toBigInt(amount);
+    console.log(this.state.addressSigner);
+    console.log(reciver);
+    await this.dappToken.approve(this.transaction.target, value);
+    await this.transaction.sendTransaction(reciver, value, message, {
+      from: this.state.addressSigner,
+      value: value,
+      gas: 200000000,
+    });
+  };
 
-  Swap = async (event) =>{
+  Swap = async (event) => {
     event.preventDefault();
-  //  console.log(this.exchange)
+    //  console.log(this.exchange)
     const amount = event.target.tokensExchange.value;
-   const value = ethers.toBigInt(amount);
+    const value = ethers.toBigInt(amount);
 
-   try {
-    // Approve the exchange to spend tokens on behalf of the user
-    await this.dappToken.approve(this.dappTokenSale.target, value, {
+    try {
+      // Approve the exchange to spend tokens on behalf of the user
+      await this.dappToken.approve(this.dappTokenSale.target, value, {
         from: this.state.addressSigner,
-       
-        gas: 20000000,
-    });
 
-    // Sell tokens on the exchange
-    await this.dappTokenSale.sellTokens(value, {
+        gas: 20000000,
+      });
+
+      // Sell tokens on the exchange
+      await this.dappTokenSale.sellTokens(value, {
         from: this.state.addressSigner,
         value: value,
         gas: 20000000,
-    });
+      });
 
-    console.log("Tokens approved and sold successfully!");
-} catch (error) {
-    console.error("Error during token approval and sale:", error);
-}
-      
+      console.log("Tokens approved and sold successfully!");
+    } catch (error) {
+      console.error("Error during token approval and sale:", error);
+    }
+  };
+
+  AirDrop = async(event) =>{
+      event.preventDefault();
+    await this.airDrop.claim(this.state.addressSigner);
     
-  
+    
+    ;
   }
+
 
   polling = () => {
     this.pollingInterval = setInterval(async () => {
-     
-      const tokensAvailable = await this.dappToken.balanceOf(this.dappTokenSale.target);
+      const tokensAvailable = await this.dappToken.balanceOf(
+        this.dappTokenSale.target
+      );
       const tokensSold = await this.dappTokenSale.tokensSold();
-        this.setState({ tokensSold });
-        this.setState({tokensAvailable});
+      this.setState({ tokensSold });
+      this.setState({ tokensAvailable });
     }, 1000); // Poll every 1 second
-};
-
-
+  };
 
   componentWillUnmount() {
     // Remove the event listener when the component unmounts
-   this.loadLogo();
-   this.polling();
+    this.loadLogo();
+    this.polling();
   }
 
   render() {
-    const {
-      addressSigner,
-      loading,
-      tokenPrice,
-      tokensSold,
-      tokensAvailable,
-    } = this.state;
+    const { addressSigner, loading, tokenPrice, tokensSold, tokensAvailable } =
+      this.state;
 
     return (
       <div className="App">
@@ -363,7 +352,7 @@ class App extends Component {
                     required
                   />
                 </div>
-                <button type="submit"  className="btn btn-primary">
+                <button type="submit" className="btn btn-primary">
                   Buy Tokens
                 </button>
               </form>
@@ -391,49 +380,49 @@ class App extends Component {
                   placeholder="Amount of Tokens"
                   required
                 />
-             
+
                 <button type="submit" className="btn btn-primary">
                   Wyślij
                 </button>
               </form>
-              
             </div>
-      
           </div>
         </div>
-        <br/>
-        <br/>
-       
+        <br />
+        <br />
+
         <div className="container">
           <div className="row">
             <div className="col-md-8">
               <p>Transactions:</p>
               <form onSubmit={this.Swap}>
-          <input  type="text"
+                <input
+                  type="text"
                   id="tokensExchange"
                   className="form-control"
                   placeholder="TokensExchange"
-                  required/>
-                  <br/>
-                  <span className="float-right text-muted">
-            Balance: {(this.tokenPrice)}
-          </span>
-          <br/>
-          <input
-           type="text"
-           id="etherExchange"
-           className="form-control"
-           placeholder="0"
-           value={this.tokenPrice}
-            disabled
-           />
-            <br/>
-             <button type="submit" className="btn btn-primary">
-                 Wymień
+                  required
+                />
+                <br />
+                <span className="float-right text-muted">
+                  Balance: {this.tokenPrice}
+                </span>
+                <br />
+                <input
+                  type="text"
+                  id="etherExchange"
+                  className="form-control"
+                  placeholder="0"
+                  value={this.tokenPrice}
+                  disabled
+                />
+                <br />
+                <button type="submit" className="btn btn-primary">
+                  Wymień
                 </button>
-           </form>
-        </div>
-        </div>
+              </form>
+            </div>
+          </div>
         </div>
       </div>
     );
